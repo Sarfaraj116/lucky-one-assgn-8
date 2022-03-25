085a9d25b9258cdeeade6a58cd1206334b256734
Sarfaraj116 <sarfarajshaikat116@outlook.com>
2022-03-26 00:04:02 +0600

    experiment

--- src/App.js
+++ src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello Baby</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ipsa, aut ipsam aspernatur dignissimos architecto, temporibus ipsum nemo repudiandae officiis cum. Aperiam modi fugiat cupiditate eveniet, velit natus harum quod cumque. Voluptate, ullam reiciendis est vitae exercitationem recusandae suscipit minus.</p>
     </div>
   );
 }
