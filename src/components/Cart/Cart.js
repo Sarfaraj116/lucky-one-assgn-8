2e38322d06e0cdf612427b76261acad8d81ffe4a
Sarfaraj116 <sarfarajshaikat116@outlook.com>
2022-03-27 02:05:48 +0600

    all added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -12,13 +12,13 @@ const Cart = ({ cart }) => {
     const finalTotalPrice = total + tax;
     return (
         <div className='fixed-cart'>
-            <h3>Show here selected mobile:</h3>
+            <h3>Showing selected Products :</h3>
             <img src={Image} alt="" srcset="" />
             <h3>Selected Item: {cart.length}</h3>
             <h3>Total price : $ {total}</h3>
             <h3>Tax : $ {tax}</h3>
             <h3>Final price : $ {finalTotalPrice.toFixed(2)}</h3>
-            <button>Choose one mobile</button><br></br>
+            <button>Choose one Product</button><br></br>
             <button>Choose Again</button>
         </div>
 
