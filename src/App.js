e6e5fa0526d91c84bbf3fb30c194a18867f72587
Sarfaraj116 <sarfarajshaikat116@outlook.com>
2022-03-26 21:46:19 +0600

    new commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <h1>hello Baby</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ipsa, aut ipsam aspernatur dignissimos architecto, temporibus ipsum nemo repudiandae officiis cum. Aperiam modi fugiat cupiditate eveniet, velit natus harum quod cumque. Voluptate, ullam reiciendis est vitae exercitationem recusandae suscipit minus.</p>
+      <p>My name is khan</p>
     </div>
   );
 }
